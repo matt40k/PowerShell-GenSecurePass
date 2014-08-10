e5707c0dfe7eb13e773a39952bd4ff9f8c225db8
Matt Smith <matt@matt40k.co.uk>
2014-08-10 01:40:59 +0100

    Initial check-in

---
+++ PowerShell-GenSecurePass.ps1
@@ -0,0 +1,31 @@
+﻿# 
+# PowerShell-GenSecurePass
+# ====================================================
+# Generates a random password with upper and lower 
+# case alphanumeric and at least 1 special character
+#
+# Author: Matt Smith (https://matt40k.uk)
+# Date: 10/08/2015
+# Version 0.a
+# License: GPL v2
+#
+
+Function GenSecurePass([int]$PasswordLength)
+{
+  $len = 24
+  if ($PasswordLength -ge 12)
+  {
+    $len = $PasswordLength
+  }
+  $chars = [Char[]]"012@;:,.<>#STUV3789qr4LMNOIJKcdABP6ab0I-CDEO1*23uvwxyzbcdefstuCDQR(_+IJKLMabvwxA)yzEWXYZF=BGHef!£^ghijklmnop56N4TUV$%WXRSYZ5678klmnopqrs0abcdeFGHOTfghij1290123agPQRSLMNFGHIJK678EFGHPQ9defghij456UVWXYZ789klopqrst3mn45chi7LMNJKABCDOPQRjklmnopqrstuvwS89TUVWxyzXYZ012ABC012345DE3456789"
+  $genpass = ($chars | Get-Random -Count $len) -join ""
+  if
+    (($genpass.Contains("@")) -or ($genpass.Contains(';'))-or ($genpass.Contains(':')) -or ($genpass.Contains(',')) -or ($genpass.Contains('.')) -or ($genpass.Contains('<')) -or ($genpass.Contains('>')) -or ($genpass.Contains('#')) -or ($genpass.Contains('-')) -or ($genpass.Contains('*')) -or ($genpass.Contains('(')) -or ($genpass.Contains('_')) -or ($genpass.Contains('+'))  -or ($genpass.Contains(')'))  -or ($genpass.Contains('='))  -or ($genpass.Contains('!'))  -or ($genpass.Contains('£'))  -or ($genpass.Contains('^')) -or ($genpass.Contains('$'))  -or ($genpass.Contains('%'))  -or ($genpass.Contains('"')))
+  {
+    return $genpass
+  }
+  else
+  {
+    return GenPass
+  }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+PowerShell-GenSecurePass
+========================
+Generates a random password with upper and lower case alphanumeric characters and at least 1 special character.
+
+Default length - 24 characters
+Min length - 12 characters
+
+Author: Matt Smith [(https://matt40k.uk)](https://matt40k.uk)
+Date: 10/08/2015
+Version 0.a
+License: GPL v2
+
+##Example of usage:
+
+```
+$Secure_Password = GenSecurePass
+Write-Host $Secure_Password
+```
+Example result: jQ5Dw0pWDi2_j7mITDI2JWk3
+
+
+Setting the PasswordLength to 16 characters
+```
+$Secure_Password = GenSecurePass -PasswordLength 16
+Write-Host $Secure_Password
+```
+Example result: yzzQV0G4*qxLY2X6
+
+Attempting to set the PasswordLength to lower then 12 will be ignored and the default (24) character length will be used.
+
+```
+$Secure_Password = GenSecurePass -PasswordLength 10
+Write-Host $Secure_Password
+```
+Example result: Ykqa02s2cMug^1D9rSatAGeJ
